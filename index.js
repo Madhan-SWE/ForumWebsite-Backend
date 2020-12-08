5e449e9bf2da7c2efddcb46cad2c8c2068ca831c
madhan-swe <madhankumarcvkl@gmail.com>
2020-12-08 23:38:11 +0530

    modified api functionalities

--- index.js
+++ index.js
@@ -73,7 +73,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// [authorizeUser, allowPermittedUser()],
 
 app.post("/topics", async (req, res) => {
     try {
@@ -89,7 +88,11 @@ app.post("/topics", async (req, res) => {
 
         let result = await db.collection("topics").findOne({topic: req.body.topic});
         if (result) {
-            res.status(409).json({result: false, message: "Topic Already exists! Please create a new topic."});
+            res.status(409).json({
+                result: false, 
+                message: "Topic Already exists! Please create a new topic.",
+                status: 409
+            });
             return;
         }
         data.createdOn = new Date();
@@ -99,131 +102,115 @@ app.post("/topics", async (req, res) => {
         console.log(data);
 
         result = await db.collection("topics").insertOne(data);
-        res.status(200).json({message: "Topic added successfully", result: true});
+        res.status(200).json({
+            message: "Topic added successfully", 
+            result: true,
+            status: 200
+        });
     } catch (err) {
         console.log(err);
-        res.status(500).json({message: "Internal server error", result: false});
+        res.status(500).json({
+            message: "Internal server error", 
+            result: false,
+            status: 500
+        });
     }
 });
 
-
+/* route to add topic */
 app.post("/topics/:topicId", async (req, res) => {
     try {
         let client = await mongodb.connect(dbUrl);
         let db = client.db("ForumWebsiteDB");
-        let data = req.body;
-        let fromDate = req.query.fromDate;
-        console.log("FromDate:", fromDate);
-        const url = require('url');
-        const querystring = require('querystring');
-
-        let parsedQs = querystring.parse(req._parsedUrl.query);
-        console.log("--",parsedQs);
-        console.log(req.query)
-
-        // let authHeader = req.headers.authorization;
-        // let decodedHeader = jwt_decode(authHeader);
-        // let email = decodedHeader.email;
-        // let userName = decodedHeader.userName;
-        // let email = "rcmadhankumar@gmail.com";
-        // let userName = "Madhankumar Chellamuthu";
-        // let topicId = ObjectId(req.params.topicId);
+        let topicId = ObjectId(req.params.topicId)
 
         
-        // data.createdOn = new Date();
-        // // data.email = email;
-        // // data.userName = userName;
-        // console.log(data);
-
-        let filter = {};
-        // if(req.query.fromDate)
-        // {
-        //     filter.discussion.createdOn = {
-        //         $gte: new Date(req.query.fromDate)
-        //     }
-        // }
+        let email = "rcmadhankumar@gmail.com";
+        let userName = "Madhankumar Chellamuthu";
         
-        // if(req.query.toDate)
-        // {
-        //     filter = {
-        //         "discussion.createdOn":{  $eq: new Date(req.query.toDate)}
-        //     }
-                
-        // }
-        
-        /*let result = db.collection("topics").aggregate({}).project({
-            discussion:{
-                $filter: {
-                    input: "$discussion",
-                    cond: {
-                        $and:[
-                            // {
-                            //     $lte : ["$$this.createdOn", new Date(req.query.toDate)]
-                            // },
-                            // {
-                            //     $gte : ["$$this.createdOn", new Date(req.query.fromDate)]
-                            // }
-                        ]
-                    }
-                }
-            },
-            
-        })*/
+        //check if topic exists
+        let result = await db.collection("topics").findOne({_id: topicId});
+        if(! result)
+        {
+            res.status(404).json({
+                message: "Topic not found",
+                result: false,
+                status: 404
+            })
+            return
+        }
 
-        /*let result = db.collection("topics").find({
-            name: {
-                $regex: '^Py'
+        let reply = req.body.reply;
+
+        let data = {
+            createdOn : new Date(),
+            reply,
+            email,
+            userName
+        }
+        
+
+        // Add reply to discussion
+        result = await db.collection("topics").findOneAndUpdate({
+            _id: topicId
+        },
+        {
+            $push: {
+                discussion: data
             }
-        })
-        */
+        });
 
-        /*let result = db.collection("topics").updateOne({
-            "discussion.userName": "Madhankumar Chellamuthu"
-        },{
-            $set: {
-                "discussion.$.userName": "Maddy"
-            }
-        })*/
+        res.status(200).json({
+            message: "Reply added", 
+            result: true,
+            status: 200
+        });
 
-        /*let result = db.collection("topics").updateOne({},{
-            $pull: {
-                discussion: {
-                    userName: "AhemedKoushik"
-                }
-            }
-        })*/
-
-        ///console.log(new Date(req.query.toDate))
-        // console.log(filter)
-        // let result = await db.collection("topics").find().project({
-        //     discussion:1
-        // }, {
-        //     filter
-        // })
-
-
-
-        // {"discussion": 1}
-        // , filter);
-        //result.limit(2)
-        // result = (await result.limit(2).toArray())
-        console.log(result)
-        //console.log(result[0].discussion)
-        // console.log(result[0].discussion)
-        
-        
-
-        //result = await db.collection("topics").findOneAndUpdate({
-        //     _id: topicId
-        // },
-        // {
-        //     $push: {
-        //         discussion: data
-        //     }
-        // });
-        res.status(200).json({message: "Topic added successfully", result: true});
     } catch (err) {
         console.log(err);
-        res.status(500).json({message: "Internal server error", result: false});
+        res.status(500).json({
+            message: "Internal server error", 
+            result: false,
+            status: 500
+        });
+    }
+});
+
+/* route to get content by topicId */
+app.get("/topics/:topicId", async (req, res) => {
+    try {
+        let client = await mongodb.connect(dbUrl);
+        let db = client.db("ForumWebsiteDB");
+        let topicId = ObjectId(req.params.topicId)
+        
+        //get content
+        let result = await db.collection("topics").findOne({_id: topicId});
+        if(! result)
+        {
+            // return 404 if topic doesn't exist
+            res.status(404).json({
+                message: "Topic not found",
+                result: false,
+                status: 404
+            })
+            return
+        }
+
+        console.log(result);
+
+        // return content
+        res.status(200).json({
+            body: result, 
+            result: true,
+            status: 200
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Internal server error", 
+            result: false,
+            status: 500
+        });
     }
 });
